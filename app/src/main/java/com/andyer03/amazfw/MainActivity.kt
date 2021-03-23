b5c49b4c437bf1dd9d7da6ad1b44bbea22bb3944
AndyER03 <65981689+AndyER03@users.noreply.github.com>
2021-03-24 01:09:20 +0300

    Code cleanup

--- app/src/main/java/com/andyer03/amazfw/MainActivity.kt
+++ app/src/main/java/com/andyer03/amazfw/MainActivity.kt
@@ -10,16 +10,14 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.view.Menu
-import android.view.MenuItem
+import android.view.KeyEvent
 import android.webkit.*
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
-    private val url: String = "https://schakal.ru/fw/firmwares_list.htm"
-
+    private val url = "https://schakal.ru/fw/firmwares_list.htm"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,27 +30,24 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         val webView = findViewById<WebView>(R.id.webView)
         webView.loadUrl(url)
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            webSettings.forceDark = WebSettings.FORCE_DARK_AUTO
+        }
         webView.webViewClient = object : WebViewClient() {
             override fun onReceivedError(webView: WebView, errorCode: Int, description: String, failingUrl: String) {
-                try {
-                    webView.stopLoading()
-                } catch (e: Exception) {
-                }
-                if (webView.canGoBack()) {
-                    webView.goBack()
-                }
                 webView.loadUrl("about:blank")
                 val alertDialog = AlertDialog.Builder(this@MainActivity).create()
                 alertDialog.setTitle(getString(R.string.error))
                 alertDialog.setMessage(getString(R.string.retry_connect))
-                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.retry)) { _, _ ->
+                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.back)) { _, _ ->
                     alertDialog.dismiss()
                     webView.reload()
                     webView.loadUrl(url)
                 }
                 alertDialog.setCancelable(false)
                 alertDialog.show()
-                super.onReceivedError(webView, errorCode, description, failingUrl)
             }
         }
         webView.setDownloadListener { url, userAgent, contentDisposition, mimeType, _ ->
@@ -60,7 +55,7 @@ class MainActivity : AppCompatActivity() {
             request.setMimeType(mimeType)
             request.addRequestHeader("cookie", CookieManager.getInstance().getCookie(url))
             request.addRequestHeader("User-Agent", userAgent)
-            request.setDescription("Downloading file...")
+            request.setDescription(getString(R.string.downloading))
             request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType))
             request.allowScanningByMediaScanner()
             request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
@@ -69,49 +64,28 @@ class MainActivity : AppCompatActivity() {
             dm.enqueue(request)
             Toast.makeText(applicationContext, getString(R.string.downloading), Toast.LENGTH_LONG).show()
         }
-        val webSettings = webView.settings
-        webSettings.javaScriptEnabled = true
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            webSettings.forceDark = WebSettings.FORCE_DARK_AUTO
-        }
-    }
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
-        R.id.about_button -> {
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle(getString(R.string.about))
-            builder.setMessage(getString(R.string.app_name)+
-                    " "+
-                    BuildConfig.VERSION_NAME+
-                    "\n\n"+
-                    getString(R.string.logic_credits)+
-                    "\n"+
-                    getString(R.string.app_credits))
-            builder.setPositiveButton(android.R.string.yes) { dialog, _ ->
-                dialog.cancel()
-            }
-            builder.show()
-            true
-        }
-        R.id.exit_button -> {
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle(getString(R.string.exit_title))
-            builder.setMessage(getString(R.string.exit_content))
-            builder.setPositiveButton(android.R.string.yes) { _, _ ->
-                super.onBackPressed()
-            }
-            builder.setNegativeButton(android.R.string.no) { dialog, _ ->
-                dialog.cancel()
-            }
-            builder.show()
-            true
-        }
-        else -> super.onOptionsItemSelected(item)
     }
     override fun onBackPressed() {
         findViewById<WebView>(R.id.webView).loadUrl(url)
+    }
+    override fun onKeyLongPress(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(getString(R.string.app_name)+
+                    " "+
+                    BuildConfig.VERSION_NAME)
+            builder.setMessage(getString(R.string.logic_credits)+
+                    "\n"+
+                    getString(R.string.app_credits))
+            builder.setPositiveButton(getString(R.string.exit_title)) { _, _ ->
+                super.onBackPressed()
+            }
+            builder.setNegativeButton(getString(R.string.back)) { dialog, _ ->
+                dialog.cancel()
+            }
+            builder.show()
+            return true
+        }
+        return super.onKeyLongPress(keyCode, event)
     }
 }
